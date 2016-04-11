a95a3b807730a5c2046f6762b20a59071325fb49
Eugene Hochenedel <eugeneatschool@gmail.com>
2016-04-11 11:50:07 -0500

    Movement and Just Cause
    Began writing player movement and to backup files.

--- 8HourGame2/Assets/_Scripts/PlayerMovement.cs
+++ 8HourGame2/Assets/_Scripts/PlayerMovement.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
+	public float Velocity;
+
+	public float CameraSensitivity;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +14,25 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		float VerticalLook = -Input.GetAxis("Mouse Y") * CameraSensitivity;
+		transform.Rotate(VerticalLook, 0, 0);
+
+		float HorizontalLook = Input.GetAxis("Mouse X") * CameraSensitivity;
+		transform.Rotate(0, HorizontalLook, 0);
+
+		float ForwardMove = Input.GetAxis("Vertical") * Velocity;
+
+		float StrafeMove = Input.GetAxis("Horizontal") * Velocity;
+
+		float VertMove = Input.GetAxis("Jump") * Velocity;
+
+		Vector3 speed = new Vector3(StrafeMove, VertMove, ForwardMove);
+		speed = transform.localRotation * speed;
+
+		CharacterController playerMove = GetComponent<CharacterController>();
+		playerMove.Move(speed);
+
+
 	}
 }
